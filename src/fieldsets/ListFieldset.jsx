9061d33ccb43053de121bb88050b8e197c9414df
zhangkun215451 <978895036@qq.com>
2019-12-27 18:57:01 +0800

    formatting

--- src/fieldsets/ListFieldset.jsx
+++ src/fieldsets/ListFieldset.jsx
@@ -3,70 +3,62 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { polyfill, } from 'react-lifecycles-compat';
+import { polyfill } from 'react-lifecycles-compat';
 
 import FormField from '../FormField';
 import FieldConverter from '../FieldConverter';
-import Select, { Option, } from 'rc-select';
-import Collapse, { Panel, } from 'rc-collapse';
+import Select, { Option } from 'rc-select';
+import Collapse, { Panel } from 'rc-collapse';
 import SubmitButton from '../common/SubmitButton';
-import {flattenItemRules, } from '../common/utils';
+import { flattenItemRules } from '../common/utils';
 import isEmpty from 'lodash/isEmpty';
 import range from 'lodash/range';
 
+const getCurrentLength = props => {
+    const { value = [], length } = props;
 
-const getCurrentLength = (props) => {
-    const {value = [], length, } = props;
-
-    if(typeof length === 'number'){
+    if (typeof length === 'number') {
         return length;
-    }
-    else if(Array.isArray(value) && !isEmpty(value)){
+    } else if (Array.isArray(value) && !isEmpty(value)) {
         return value.length;
-    }
-    else if(Array.isArray(length)){
+    } else if (Array.isArray(length)) {
         return length[0];
-    }
-    else{
+    } else {
         throw new Error('unexpected length type in props');
     }
 };
 
-class ListFieldset extends React.Component{
-
+class ListFieldset extends React.Component {
     static propTypes = {
         name: PropTypes.string.isRequired,
         numLabel: PropTypes.string,
-        length: PropTypes.oneOfType([
-            PropTypes.number.isRequired,
-            PropTypes.array.isRequired,
-        ]),
+        length: PropTypes.oneOfType([PropTypes.number.isRequired, PropTypes.array.isRequired]),
         value: PropTypes.any,
         fields: PropTypes.array,
         // 折叠面板标题
         panelTitle: PropTypes.string,
         // 是否可以分段保存提交
         submit: PropTypes.bool,
-    }
+    };
 
     static defaultProps = {
         labelWidth: 140,
         submit: false,
-    }
+    };
 
-    static getDerivedStateFromProps(nextProps){
-        if('value' in nextProps){
-            const {length, value, } = nextProps;
+    static getDerivedStateFromProps(nextProps) {
+        if ('value' in nextProps) {
+            const { length, value } = nextProps;
             return {
                 value: value || [],
                 // 仅当长度为 数字时才硬性指定
-                ...(typeof length === 'number' ? {currentLength: length, } : null),
+                ...(typeof length === 'number' ? { currentLength: length } : null),
             };
         }
         return null;
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -79,84 +71,79 @@ class ListFieldset extends React.Component{
     }
 
     validate = () => {
-        const {currentLength, } = this.state;
+        const { currentLength } = this.state;
         const isValid = range(currentLength).reduce((acc, index) => {
             const groupFields = this.refFields[index];
-            return Object.keys(groupFields)
-                .reduce((suc, key) => groupFields[key].validate() && suc, true) && acc;
-
+            return Object.keys(groupFields).reduce((suc, key) => groupFields[key].validate() && suc, true) && acc;
         }, true);
 
-        if(!isValid){
-            this.setState({activePanelKey: '0', });
+        if (!isValid) {
+            this.setState({ activePanelKey: '0' });
         }
         return isValid;
-    }
+    };
 
     getValue = () => {
-        const {currentLength, } = this.state;
+        const { currentLength } = this.state;
 
-        return range(currentLength)
-            .map((index) => {
-                const groupFields = this.refFields[index];
-                return Object.keys(groupFields)
-                    .reduce((acc, key) => ({...acc, [key]: groupFields[key].getValue(), }), {});
-            });
-    }
+        return range(currentLength).map(index => {
+            const groupFields = this.refFields[index];
+            return Object.keys(groupFields).reduce((acc, key) => ({ ...acc, [key]: groupFields[key].getValue() }), {});
+        });
+    };
 
     renderSelectField = () => {
-        const {currentLength, } = this.state;
-        const {length, numLabel, } = this.props;
+        const { currentLength } = this.state;
+        const { length, numLabel } = this.props;
 
-        return Array.isArray(length)
-            ?  <FormField
+        return Array.isArray(length) ? (
+            <FormField
                 label={numLabel}
                 value={currentLength + ''}
-                onChange={(currentLength) => {
+                onChange={currentLength => {
                     this.setState({
                         currentLength,
                         activeTabKey: 0,
                     });
                 }}
             >
-                <Select style={{ width: 100, }}>
-                    {length.map((tabLength) =>
-                        <Option key={tabLength} value={tabLength}>{tabLength}</Option>
-                    )}
+                <Select style={{ width: 100 }}>
+                    {length.map(tabLength => (
+                        <Option key={tabLength} value={tabLength}>
+                            {tabLength}
+                        </Option>
+                    ))}
                 </Select>
             </FormField>
-        // 固定长度的 不显示长度选择控件
-            : null;
+        ) : // 固定长度的 不显示长度选择控件
+            null;
     };
 
-
     handleSubmit = () => {
-        if(!this.validate()) return;
+        if (!this.validate()) return;
 
-        const {onSubmit, name: itemName, } = this.props;
+        const { onSubmit, name: itemName } = this.props;
 
-        if(onSubmit){
+        if (onSubmit) {
             onSubmit({
                 [itemName]: this.getValue(),
             });
         }
     };
 
-    render(){
-        const {currentLength, value, } = this.state;
-        const {context, labelWidth, submit, panelTitle, fields, onChange, length, } = this.props;
+    render() {
+        const { currentLength, value } = this.state;
+        const { context, labelWidth, submit, panelTitle, fields, onChange, length } = this.props;
         // 所选 length 在数组中的下标
         // 这里实现了一些特殊的业务逻辑，当 tab 的子组件的 rules 字段为数组时，则根据 tab 个数来觉得取得哪个 rules 的值
         // 这个逻辑是为了解决以下场景的需要，例如要显示一个表格，当列数为 3 时则表头字数限制为 24，当列数为 4 时，则字数限制为 18
-        const ruleIndex = Array.isArray(length)
-            ? length.indexOf(currentLength)
-            : 0;
+        const ruleIndex = Array.isArray(length) ? length.indexOf(currentLength) : 0;
         const listIndexList = range(currentLength);
-        const listGroups = listIndexList.map((listIndex) => {
+        const listGroups = listIndexList.map(listIndex => {
             const groupValue = value[listIndex] || {};
 
-            return fields.map((item) => {
-                const {name: itemName, label, } = item;
+            return fields.map(item => {
+                const { name: itemName, label } = item;
                 const fieldProps = flattenItemRules(item, ruleIndex);
 
                 return (
@@ -167,26 +154,24 @@ class ListFieldset extends React.Component{
                         key={`${itemName}-${listIndex}`}
                         label={label + (listIndex + 1)}
                         value={groupValue[itemName]}
-                        fieldRef={(field) => {
-                            if(!this.refFields[listIndex]){
+                        fieldRef={field => {
+                            if (!this.refFields[listIndex]) {
                                 this.refFields[listIndex] = {};
                             }
                             this.refFields[listIndex][itemName] = field;
                         }}
-                        onChange={(changedValue) => {
-                            const updater = (state) => ({
-                                value: listIndexList.map((index) => {
+                        onChange={changedValue => {
+                            const updater = state => ({
+                                value: listIndexList.map(index => {
                                     const value = state.value[index] || {};
-                                    return index === listIndex
-                                        ? {...value, [itemName]: changedValue, }
-                                        : value;
+                                    return index === listIndex ? { ...value, [itemName]: changedValue } : value;
                                 }),
                             });
 
-                            if(!('value' in this.props)){
+                            if (!('value' in this.props)) {
                                 this.setState(updater);
                             }
-                            if(onChange){
+                            if (onChange) {
                                 onChange(updater(this.state).value);
                             }
                         }}
@@ -202,15 +187,18 @@ class ListFieldset extends React.Component{
             </div>
         );
 
-        return panelTitle
-            ? <Collapse activeKey={this.state.activePanelKey} onChange={(activePanelKey) => {
-                this.setState({activePanelKey, });
-            }}>
-                <Panel header={panelTitle}>
-                    {fieldset}
-                </Panel>
+        return panelTitle ? (
+            <Collapse
+                activeKey={this.state.activePanelKey}
+                onChange={activePanelKey => {
+                    this.setState({ activePanelKey });
+                }}
+            >
+                <Panel header={panelTitle}>{fieldset}</Panel>
             </Collapse>
-            : fieldset;
+        ) : (
+            fieldset
+        );
     }
 }
 
--- src/fieldsets/RadioFieldset.jsx
+++ src/fieldsets/RadioFieldset.jsx
@@ -3,29 +3,26 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { polyfill, } from 'react-lifecycles-compat';
+import { polyfill } from 'react-lifecycles-compat';
 
 import FormField from '../FormField';
 import RadioGroup from '../controls/RadioGroup';
 import FieldConverter from '../FieldConverter';
 import SubmitButton from '../common/SubmitButton';
-import Collapse, { Panel, } from 'rc-collapse';
+import Collapse, { Panel } from 'rc-collapse';
 import findIndex from 'lodash/findIndex';
 
-
-const getRadioValue = ({value, optionFields, radioValueKey, defaultRadio, }) => {
-    if(value && value[radioValueKey]){
+const getRadioValue = ({ value, optionFields, radioValueKey, defaultRadio }) => {
+    if (value && value[radioValueKey]) {
         return value;
-    }
-    else if(defaultRadio !== undefined){
-        return {...value, [radioValueKey]: defaultRadio, };
-    }
-    else{
-        return {...value, [radioValueKey]: optionFields[0].option.value, };
+    } else if (defaultRadio !== undefined) {
+        return { ...value, [radioValueKey]: defaultRadio };
+    } else {
+        return { ...value, [radioValueKey]: optionFields[0].option.value };
     }
 };
 
-class RadioFieldset extends React.Component{
+class RadioFieldset extends React.Component {
     static propTypes = {
         // 字段组名
         name: PropTypes.string.isRequired,
@@ -34,34 +31,27 @@ class RadioFieldset extends React.Component{
         // radio label
         radioLabel: PropTypes.string,
         // 用于存储选项值的字段名
-        radioValueKey: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number,
-        ]),
-        defaultRadio: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number,
-        ]),
-        optionFields: PropTypes.arrayOf(PropTypes.shape({
-            option: PropTypes.oneOfType([
-                PropTypes.string.isRequired,
-                PropTypes.object.isRequired,
-            ]),
-            fields: PropTypes.any.isRequired,
-        })),
+        radioValueKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        defaultRadio: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        optionFields: PropTypes.arrayOf(
+            PropTypes.shape({
+                option: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.object.isRequired]),
+                fields: PropTypes.any.isRequired,
+            }),
+        ),
         // 折叠面板标题
         panelTitle: PropTypes.string,
         // 是否可以分段保存提交
         submit: PropTypes.bool,
-    }
+    };
 
     static defaultProps = {
         radioValueKey: 'radioValue',
         labelWidth: 140,
         submit: false,
-    }
+    };
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -72,8 +62,8 @@ class RadioFieldset extends React.Component{
         this.refFields = {};
     }
 
-    static getDerivedStateFromProps(nextProps){
-        if('value' in nextProps){
+    static getDerivedStateFromProps(nextProps) {
+        if ('value' in nextProps) {
             return {
                 value: getRadioValue(nextProps),
             };
@@ -86,63 +76,76 @@ class RadioFieldset extends React.Component{
             const valid = this.refFields[key].validate();
             return suc && valid;
         }, true);
-        if(!resValid){
-            this.setState({activePanelKey: '0', });
+        if (!resValid) {
+            this.setState({ activePanelKey: '0' });
         }
         return resValid;
     };
 
     getValue = () => {
-        return Object.keys(this.refFields)
-            .reduce((acc, key) => ({...acc, [key]: this.refFields[key].getValue(), }), {});
+        return Object.keys(this.refFields).reduce(
+            (acc, key) => ({ ...acc, [key]: this.refFields[key].getValue() }),
+            {},
+        );
     };
 
     handleSubmit = () => {
-        if(!this.validate()) return;
+        if (!this.validate()) return;
 
-        const {onSubmit, name: itemName, } = this.props;
+        const { onSubmit, name: itemName } = this.props;
 
-        if(onSubmit){
+        if (onSubmit) {
             onSubmit({
                 [itemName]: this.getValue(),
             });
         }
-    }
+    };
 
-    render(){
-        const {context, labelWidth, submit, panelTitle, optionFields, radioLabel, radioValueKey, onChange, } = this.props;
-        const {value, activePanelKey, } = this.state;
+    render() {
+        const {
+            context,
+            labelWidth,
+            submit,
+            panelTitle,
+            optionFields,
+            radioLabel,
+            radioValueKey,
+            onChange,
+        } = this.props;
+        const { value, activePanelKey } = this.state;
         const radioValue = value[radioValueKey];
-        const radioIndex = findIndex(optionFields, ({option: {value, }, }) => value === radioValue);
+        const radioIndex = findIndex(optionFields, ({ option: { value } }) => value === radioValue);
         const selectedField = optionFields[radioIndex].fields || [];
         const fields = selectedField.map((item, index) => {
-            const {name: itemName, } = item;
+            const { name: itemName } = item;
 
-            return <FieldConverter
-                labelWidth={labelWidth}
-                context={context}
-                {...item}
-                key={`radio${radioIndex}-field${index}`}
-                value={value[itemName]}
-                fieldRef={(field) => {
-                    if(field){
-                        this.refFields[itemName] = field;
-                    }else{
-                        delete this.refFields[itemName];
-                    }
-                }}
-                onChange={(changedValue) => {
-                    const updater = (state) => ({
-                        value: {...state.value, [itemName]: changedValue, },
-                    });
-                    if(!('value' in this.props)){
-                        this.setState(updater);
-                    }
-                    if(onChange){
-                        onChange(updater(this.state).value);
-                    }
-                }}
-            />;
+            return (
+                <FieldConverter
+                    labelWidth={labelWidth}
+                    context={context}
+                    {...item}
+                    key={`radio${radioIndex}-field${index}`}
+                    value={value[itemName]}
+                    fieldRef={field => {
+                        if (field) {
+                            this.refFields[itemName] = field;
+                        } else {
+                            delete this.refFields[itemName];
+                        }
+                    }}
+                    onChange={changedValue => {
+                        const updater = state => ({
+                            value: { ...state.value, [itemName]: changedValue },
+                        });
+                        if (!('value' in this.props)) {
+                            this.setState(updater);
+                        }
+                        if (onChange) {
+                            onChange(updater(this.state).value);
+                        }
+                    }}
+                />
+            );
         });
 
         const fieldset = (
@@ -151,40 +154,40 @@ class RadioFieldset extends React.Component{
                     labelWidth={labelWidth}
                     label={radioLabel}
                     value={radioValue}
-                    ref={(field) => {
+                    ref={field => {
                         this.refFields[radioValueKey] = field;
                     }}
-                    onChange={(radioValue) => {
-                        const updater = (state) => ({
-                            value: {...state.value, [radioValueKey]: radioValue, },
+                    onChange={radioValue => {
+                        const updater = state => ({
+                            value: { ...state.value, [radioValueKey]: radioValue },
                         });
-                        if(!('value' in this.props)){
+                        if (!('value' in this.props)) {
                             this.setState(updater);
                         }
-                        if(onChange){
+                        if (onChange) {
                             onChange(updater(this.state).value);
                         }
                     }}
                 >
-                    <RadioGroup options={optionFields.map(({option, }) => option)} />
+                    <RadioGroup options={optionFields.map(({ option }) => option)} />
                 </FormField>
                 {fields}
                 {submit && <SubmitButton onClick={this.handleSubmit} labelWidth={labelWidth} />}
             </div>
         );
 
-        return panelTitle
-            ? <Collapse
+        return panelTitle ? (
+            <Collapse
                 activeKey={activePanelKey}
-                onChange={(activePanelKey) => {
-                    this.setState({activePanelKey, });
+                onChange={activePanelKey => {
+                    this.setState({ activePanelKey });
                 }}
             >
-                <Panel header={panelTitle}>
-                    {fieldset}
-                </Panel>
+                <Panel header={panelTitle}>{fieldset}</Panel>
             </Collapse>
-            : fieldset;
+        ) : (
+            fieldset
+        );
     }
 }
 
