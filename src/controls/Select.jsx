bcf5c174d0f6cd0d4ce26a2654eb9659640f3e28
zhangkun215451 <978895036@qq.com>
2020-01-20 14:53:49 +0800

    change select

--- src/controls/Select.jsx
+++ src/controls/Select.jsx
@@ -3,31 +3,33 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import RcSelect, { Option, } from 'rc-select';
+import RcSelect, { Option } from 'rc-select';
 
-class Select extends React.Component{
-    constructor(props){
+class Select extends React.Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
-        const {options, ...restProps} = this.props;
+    render() {
+        const { options, ...restProps } = this.props;
         let defaultValue = null;
         const optionComponents = options.map((option, index) => {
             const optionValue = typeof option === 'string' ? option : option.value;
             const optionText = typeof option === 'string' ? option : option.text;
-            if(index === 0){
+            if (index === 0) {
                 defaultValue = optionValue;
             }
             return (
-                <Option key={index} value={optionValue}>{optionText}</Option>
+                <Option key={index} value={optionValue}>
+                    {optionText}
+                </Option>
             );
         });
-        if(!restProps.value){
+        if (!restProps.value) {
             restProps.value = defaultValue;
         }
         return (
-            <RcSelect style={{ width: 200, }} {...restProps} optionLabelProp="children">
+            <RcSelect style={{ width: 200 }} {...restProps} optionLabelProp="children">
                 {optionComponents}
             </RcSelect>
         );
@@ -43,7 +45,7 @@ Select.propTypes = {
                 value: PropTypes.string,
                 text: PropTypes.string,
             }),
-        ])
+        ]),
     ),
     disabled: PropTypes.bool,
     onChange: PropTypes.func,
